6487cfff690d94983b1580c1245cc2860bed6514
seema1611 <seemarajpure16@gmail.com>
2020-04-27 20:34:19 +0530

    ADDED: Given three float numbers find maximum number

--- src/main/java/com/maximumnumber/FindMaximum.java
+++ src/main/java/com/maximumnumber/FindMaximum.java
@@ -2,9 +2,8 @@ package com.maximumnumber;
 
 public class FindMaximum {
 
-    //Method to Find Maximum Number Between three
-    public static int findMaximumNumber(int firstNumber,int secondNumber,int thirdNumber)
-    {
+    //Method to Find Maximum Number Between three Integer Numbers
+    public static int findMaximumNumber(int firstNumber,int secondNumber,int thirdNumber) {
         int maximumNumber=0;
         if(firstNumber>secondNumber && firstNumber>thirdNumber) {
             return maximumNumber=firstNumber;
@@ -15,10 +14,22 @@ public class FindMaximum {
         }
     }
 
-    //Main method
-    public static void main(String args[])
+    //Method to Find Maximum Number Between three float Numbers
+    public static double findMaximumNumber( double firstNumber,double secondNumber,double thirdNumber)
     {
-        //Welcome message display
-        System.out.println("Welcome to Test Maximum");
+        double maximumNumber=0.0;
+        if(firstNumber>secondNumber && firstNumber>thirdNumber) {
+            return maximumNumber=firstNumber;
+        }
+        else if (secondNumber>firstNumber && secondNumber>thirdNumber) {
+            return maximumNumber=secondNumber;
+        } else {
+            return maximumNumber=thirdNumber;
+        }
+    }
+
+    //Main method
+    public static void main(String args[]) {
+        System.out.println("Welcome to findout Maximum Number");
     }
 }
--- src/test/java/com/maximumnumber/test/FindMaximumTest.java
+++ src/test/java/com/maximumnumber/test/FindMaximumTest.java
@@ -8,24 +8,44 @@ public class FindMaximumTest {
     FindMaximum findMaximum=new FindMaximum();
 
     //UseCase-1
-    //TC-1
+    //TC-1.1
     @Test
     public void givenMaximumNumber_atFirstPosition_shouldReturnFirstNumber() {
         Comparable maximumNumber=findMaximum.findMaximumNumber(80,50,30);
         Assert.assertEquals(80,maximumNumber);
     }
 
-    //TC-2
+    //TC-1.2
     @Test
     public void givenMaxInteger_atSecondPosition_shouldReturnSecondPosition() {
         Comparable maximumNumber=findMaximum.findMaximumNumber(50,80,30);
         Assert.assertEquals(80,maximumNumber);
     }
 
-    //TC-3
+    //TC-1.3
     @Test
     public void givenMaxInteger_atThirdPosition_shouldReturnThirdPosition() {
         Comparable maximumNumber = findMaximum.findMaximumNumber(30, 50, 80);
         Assert.assertEquals(80, maximumNumber);
+    }
+
+    //UseCase-2
+    //TC:2.1
+    @Test
+    public void givenMaxFloat_atFirstPosition_shouldReturnFirstPosition() {
+        Comparable maximumNumber = findMaximum.findMaximumNumber(80.6,50.8,30.2);
+        Assert.assertEquals(80.6, maximumNumber);
+    }
+    //TC:2.2
+    @Test
+    public void givenMaximumFloat_atSecondPosition_shouldReturnFirstPosition() {
+        Comparable maximumNumber = findMaximum.findMaximumNumber(30.2,80.6,50.8);
+        Assert.assertEquals(80.6, maximumNumber);
+    }
+    //TC:2.3
+    @Test
+    public void givenMaxFloat_atThirdPosition_shouldReturnFirstPosition() {
+        Comparable maximumNumber = findMaximum.findMaximumNumber(30.2, 50.8, 80.6);
+        Assert.assertEquals(80.6, maximumNumber);
     }
 }
